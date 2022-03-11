a0f76db051b4ad447b8ad09352318d92865e56e5
douglaslimab <douglaslimab@gmail.com>
2022-03-11 19:14:56 +0000

    uart test

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import serial
+
+uart2 = serial.Serial(port='/dev/ttyAMA1', baudrate=9600)
+uart4 = serial.Serial(port='/dev/ttyAMA2', baudrate=9600)
+
+def write_2(msg):
+    uart2.write(bytes(msg, 'utf-8'))
+
+def read_2():
+    return uart2.readline()
+
+def write_4(msg):
+    uart4.write(bytes(msg, 'utf-8'))
+
+def read_4():
+    return  uart4.readline()
+
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
